b3f7cf6cffc4fdcaa63ffd6f85392cb9dc9e9ce5
Ilia <sirtsevilya@yandex.ru>
2021-02-04 18:51:04 +0300

    bracket 1.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Bracket from './Components/Bracket/Bracket';
+import Header from './Components/Layout/Header/Header';
+import Footer from './Components/Layout/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <h1>Main page</h1>
+        </Route>
+        <Route exact path="/bracket">
+          <Bracket />
         </Route>
         <Route>
           <h1>404</h1>
