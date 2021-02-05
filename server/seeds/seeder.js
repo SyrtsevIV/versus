3a6fbdc35772e9f539d84d16df13c8e7ed889238
Ilia <sirtsevilya@yandex.ru>
2021-02-05 16:03:01 +0300

    сетка не доделана

--- server/seeds/seeder.js
+++ server/seeds/seeder.js
@@ -1,14 +1,16 @@
+/* eslint-disable no-await-in-loop */
 const mongoose = require('mongoose');
-const User = require("../models/User");
+const User = require('../models/User');
 const Stats = require('../models/Stats');
 const faker = require('faker');
+const Tournament = require('../models/Tournament');
 mongoose.connect('mongodb://localhost:27017/Versus', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
-const randomName = () => faker.name.findName(); 
+const randomName = () => faker.name.findName();
 const randomEmail = () => faker.internet.email();
 const randomPass = () => faker.internet.password();
 const randomMmr = () => faker.random.number();
@@ -18,22 +20,24 @@ const randomSilver = () => faker.random.number();
 const randomBronze = () => faker.random.number();
 
 // Seed users
-async function seedUsers() {
-  for (let i = 0; i < 30; i+=1) {
-    User.create({ 
-      login: randomName(), 
-      email: randomEmail(), 
-      password: randomPass() });
+async function seedUsers(num) {
+  for (let i = 0; i < num; i += 1) {
+    User.create({
+      login: randomName(),
+      email: randomEmail(),
+      password: randomPass(),
+    });
   }
 }
-// seedUsers();
+// seedUsers(30);
 
 // Seed stats
 async function seedStats() {
   const users = await User.find();
-  for (let i = 0; i <= users.length; i += 1) {
-    Stats.create({
-      sport: "tennis",
+  console.log('users', users);
+  for (let i = 0; i < users.length; i += 1) {
+    const stats = await Stats.create({
+      sport: 'table tennis',
       user: users[i]._id,
       mmr: randomMmr(),
       rating: randomRating(),
@@ -41,11 +45,30 @@ async function seedStats() {
       silver: randomSilver(),
       bronze: randomBronze(),
     });
+    const user = await User.findById(users[i]._id);
+    user.stats = stats._id;
+    await user.save();
   }
 }
 // seedStats();
 
 // Seed Tournaments
 
+async function seedTournament() {
+  const users = await User.find();
+  Tournament.create({
+    participants: users,
+  });
+}
+
+// seedTournament();
 
 // Seed Brackets
+
+const start = async () => {
+  await seedUsers(8);
+  await seedStats();
+  await seedTournament();
+};
+
+start();
