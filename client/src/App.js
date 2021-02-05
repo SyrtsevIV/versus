3a4a9a519a92e26dfbe388981ff723d43d131694
Kuzzarvi <Kellthuzard@gmail.com>
2021-02-05 13:35:08 +0300

    Приклеил футер к низу

--- client/src/App.js
+++ client/src/App.js
@@ -11,23 +11,21 @@ import Bracket from './Components/Bracket/Bracket';
 function App() {
   return (
     <Router>
-
-      <Header />
-
-      <Switch>
-        <Route exact path="/">
-          <Main />
-        </Route>
-        <Route path="/profile/:id">
-          <Profile />
-        </Route>
-        <Route exact path="/bracket">
-          <Bracket />
-        </Route>
-        <Route>
-          <h1>404</h1>
-        </Route>
-      </Switch>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/profile/:id">
+            <Profile />
+          </Route>
+          <Route exact path="/bracket">
+            <Bracket />
+          </Route>
+          <Route>
+            <h1>404</h1>
+          </Route>
+        </Switch>
         <Footer />
       </Router>
   );
--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -1,10 +1,10 @@
 import {Switch, Route} from 'react-router-dom';
-import "./main.module.css";
+import styles from "./main.module.css";
 import Activtournament from './Activ/Activtournament';
 
 const Main = () => {
   return (
-    <div>
+    <div className={styles.main}>
       <h1>Main page</h1>
       <Switch>
         <Route>
