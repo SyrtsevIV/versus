52c83aaf4995ede71117daa30787de6a2d87dfe6
Kuzzarvi <Kellthuzard@gmail.com>
2021-02-08 13:56:25 +0300

    поправил футер, исправил ошибку когда нет турниров

--- client/src/redux/actionCreators/mainPageStatus/getTours/getTours.js
+++ client/src/redux/actionCreators/mainPageStatus/getTours/getTours.js
@@ -3,7 +3,11 @@ import { GET_MAIN_PAGE_TOURS } from '../../../types/types';
 const getTours = (mainPageStatus) => async (dispatch) => {
   const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/tabletennis/tournament/${mainPageStatus}`);
   const result = await response.json();
-  window.history.replaceState({}, '', `/tabletennis/tournament/${result[0]?._id}`);
+  if (result.length) {
+    window.history.replaceState({}, '', `/tabletennis/tournament/${result[0]._id}`);
+  } else {
+    window.history.replaceState({}, '', `/tabletennis/tournament/`);
+  }
   dispatch({
     type: GET_MAIN_PAGE_TOURS,
     payload: result,
