c1e1e0ba39230c272cabb06d6775fc3ba1195aeb
Viktor <vict0r.frost@yandex.ru>
2021-02-11 15:08:25 +0200

    tour list and dnd

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import styles from '../Profile/profile.module.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getUserProfile } from '../../redux/actionCreators/profile';
 import { useParams } from 'react-router-dom'
@@ -14,43 +14,63 @@ import RadarDiagram from './RadarDiagram/RadarDiagram';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 
-
-
-
-
 const Profile = () => {
-  const {id} = useParams()
+  const {userId} = useParams()
   const dispatch = useDispatch()
 
+  const [arr, setArr] = useState([
+    {
+      id: '1',
+      comp: <User />
+    },
+    {
+      id: '2',
+      comp: <Rank />
+    },
+    {
+      id: '3',
+      comp: <Cup />
+    }, 
+  ])
+
   useEffect(() => {
-    dispatch(getUserProfile(id))
+    dispatch(getUserProfile(userId))
   }, [])
 
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(arr);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setArr(items);
+  }
+
+  
   return (
     <div className={styles.content}>
       
-      <DragDropContext>
-       
-        <div className={styles.topBlock}>
-          
-          {/* <Droppable droppableId="1">
-            {(provided, snapshot) => (
-              <div
-                ref={provided.innerRef}
-                style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
-                {...provided.droppableProps}
-              >
-                {provided.placeholder}
-              </div>
-            )} */}
-
-    
-            <User /> 
-            <Rank />
-            <Cup />
-          {/* </Droppable>         */}
-
-        </div>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId='menu'>
+          {(provided) => (
+            <ul className="characters" {...provided.droppableProps} ref={provided.innerRef}>
+              {arr.map(({id, comp}, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        {comp}
+                      </li>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </ul>
+          )}  
+        </Droppable>
+      </DragDropContext>          
 
         <div className={styles.statsBlock}>
 
@@ -59,9 +79,6 @@ const Profile = () => {
           <RadarDiagram />
 
         </div>
-
-      </DragDropContext>
-
     </div>
   );
 };
