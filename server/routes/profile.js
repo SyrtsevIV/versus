fb4c910ca942704d508a2f8aa0b9a2a39ae7539a
Yuriy <uradjachkov@gmail.com>
2021-02-16 13:47:28 +0300

    13.47

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const appDir = path.dirname(require.main.filename);
 
-const User = require('../models/User');
-const Stats = require('../models/Stats');
-const Match = require('../models/Match');
-const Bracket = require('../models/Bracket');
+const User = require("../models/User");
+const Stats = require("../models/Stats");
+const Match = require("../models/Match");
+const Bracket = require("../models/Bracket");
 
 const router = express.Router();
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const stats = await Stats.findOne({ user: id });
@@ -26,70 +26,79 @@ router.get('/:id', async (req, res) => {
     rating += 1;
     res.json({ stats, rating, user, allPlayerValue });
   } catch (error) {
-    res.send('Не нашел такого юзера =(');
+    res.send("Не нашел такого юзера =(");
   }
 });
 
 // Storage для хранения загруженных картинок
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, appDir + '/public/images');
+    cb(null, appDir + "/public/images");
   },
   filename: function (req, file, cb) {
-    cb(null, 'image-' + Date.now() + path.extname(file.originalname));
+    cb(null, "image-" + Date.now() + path.extname(file.originalname));
   },
 });
 
 var upload = multer({ storage });
 
-router.post('/upload/:id', upload.single('filedata'), async (req, res) => {
+router.post("/upload/:id", upload.single("filedata"), async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await User.findByIdAndUpdate(id, { avatar: req.file.filename }, { new: true });
+    const user = await User.findByIdAndUpdate(
+      id,
+      { avatar: req.file.filename },
+      { new: true }
+    );
     res.json(user);
   } catch (error) {
-    console.log('Что то пошло не так, upload');
+    res.json({ message: "Что то пошло не так" });
   }
 });
 
-router.get('/history/:id', async (req, res) => {
+router.get("/history/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const matches = await Match.find({ ended: true }).populate('player1').populate('player2');
+    const matches = await Match.find({ ended: true })
+      .populate("player1")
+      .populate("player2");
     const userMatches = matches.filter((el) => {
-      return ((String(el.player1?._id) === id || String(el.player2?._id) === id)&& el.player1 && el.player2);
+      return (
+        (String(el.player1?._id) === id || String(el.player2?._id) === id) &&
+        el.player1 &&
+        el.player2
+      );
     });
     const lastMatches = userMatches
       .slice(-3)
       .reverse()
       .map((el) => {
-        if ((el.tour === 'final')) {
-          el.tour = 'Финал';
+        if (el.tour === "final") {
+          el.tour = "Финал";
         }
-        if ((el.tour === 'thirdPlace')) {
-          el.tour = 'Матч за 3 место';
+        if (el.tour === "thirdPlace") {
+          el.tour = "Матч за 3 место";
         }
-        if ((el.tour === 'semifinal')) {
-          el.tour = 'Полуфинал';
+        if (el.tour === "semifinal") {
+          el.tour = "Полуфинал";
         }
-        if ((el.tour === 'quarterfinals')) {
-          el.tour = '1/4';
+        if (el.tour === "quarterfinals") {
+          el.tour = "1/4";
         }
-        if ((el.tour === 'fioneEighthnal')) {
-          el.tour = '1/8';
+        if (el.tour === "fioneEighthnal") {
+          el.tour = "1/8";
         }
-        if ((el.tour ==='oneSixteenth')) {
-          el.tour = '1/16';
+        if (el.tour === "oneSixteenth") {
+          el.tour = "1/16";
         }
         return el;
       });
     res.json(lastMatches);
   } catch (error) {
-    console.log(error);
-  }
+    res.json({ message: "Что то пошло не так" });  }
 });
 
-router.post('/:id', async (req, res) => {
+router.post("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const login = req.body.changeLogin;
@@ -97,23 +106,24 @@ router.post('/:id', async (req, res) => {
     const user = await User.findByIdAndUpdate(id, { login, email });
     res.json(user);
   } catch (error) {
-    // Что отправить в качестве ошибки?
-    res.send('Что то пошло не так, id');
-  }
+    res.json({ message: "Что то пошло не так" });  }
 });
 
-router.post('/customProfile/:id', async (req, res) => {
+router.post("/customProfile/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const { uniqArr } = req.body;
     console.log({ uniqArr });
-    const user = await User.findByIdAndUpdate(id, { customProfile: uniqArr }, { new: true });
+    const user = await User.findByIdAndUpdate(
+      id,
+      { customProfile: uniqArr },
+      { new: true }
+    );
     console.log(user);
     res.json({ customProfile: user.customProfile });
   } catch (error) {
     // Что отправить в качестве ошибки?
-    res.send('Что то пошло не так, id');
-  }
+    res.json({ message: "Что то пошло не так" });  }
 });
 
 module.exports = router;
